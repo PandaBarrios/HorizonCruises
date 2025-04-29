a06a30bda1e80dcf13185ce980ca90cb812cac43
208340357 <melanymatamoros19@gmail.com>
2025-04-29 15:36:00 -0600

    Cambios de ClienteDTO y menu

--- HorizonCruises.Application/DTOs/ClienteDTO.cs
+++ HorizonCruises.Application/DTOs/ClienteDTO.cs
@@ -8,24 +8,27 @@ using System.Threading.Tasks;
 
 namespace HorizonCruises.Application.DTOs
 {
-    public record ClienteDTO
+    public record ClienteDTO 
     {
-
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre completo es obligatorio.")]
         public string Nombre { get; set; } = null!;
 
-        [Display(Name = "Correo electrónico")]
-        [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido.")]
         public string CorreoElectronico { get; set; } = null!;
 
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
         [Display(Name = "Fecha de nacimiento")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateOnly FechaNacimiento { get; set; }
 
+        [Required(ErrorMessage = "El país es obligatorio.")]
         public string Pais { get; set; } = null!;
 
-        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
         [DataType(DataType.Password)]
         public string Contrasena { get; set; } = null!;
 
@@ -39,6 +42,5 @@ namespace HorizonCruises.Application.DTOs
 
         public virtual List<TelefonoDTO>? Telefono { get; set; }
         public virtual List<UsuarioHuespedDTO>? UsuarioHuesped { get; set; }
-
     }
 }
